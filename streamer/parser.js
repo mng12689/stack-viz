c2152112c1eb39dd87e8b52a840d27c47cfafbf3
Michael Ng <mng@clicktherapeutics.com>
2018-10-04 11:00:12 -0400

    write a decent streamer cli

---
+++ streamer/parser.js
@@ -0,0 +1,41 @@
+const parser = require('yargs').command('stream <host> <port>', 'Stream payloads to the specified destination', (yargs) => {
+    yargs.positional('host', {
+      describe: 'Host to which this program streams payloads',
+      type: 'string',
+      demandOption: true
+    }).positional('port', {
+      describe: 'Port of destination host',
+      type: 'string',
+      demandOption: true
+    }).options({
+      interactive: {
+        alias: 'i',
+        describe: 'Stream your own payloads on the fly in interactive mode',
+        type: 'boolean'
+      },
+      file: {
+        alias: 'f',
+        describe: 'Supply a file path to have payloads streamed line-by-line',
+        type: 'string'
+      },
+      speed: {
+        alias: 's',
+        describe: 'If in non-interactive mode, the frequency (in paylaods per sec) at which the supplied payloads are streamed to the destination',
+        type: 'number',
+        default: 1
+      },
+      nloops: {
+        alias: 'n',
+        describe: 'Number of loops to do over the input source. Works in both interactive and non-interactive modes.',
+        type: 'boolean'
+      },
+      protocol: {
+        alias: 'p',
+        describe: 'Network protocol to use for communication with destination',
+        default: 'UDP',
+        choices: [ 'UDP', 'TCP' ]
+      }
+    });
+});
+
+module.exports = parser;
---
+++ streamer/streamer.js
@@ -0,0 +1,67 @@
+const parser = require('./parser');
+const fs = require('fs');
+
+let timerId = null;
+  
+function getClient(p) {
+  // TODO: when we implement both protocols, we should expose a common interface here
+  if ( p === 'UDP' ) {
+    const dgram = require('dgram');
+    return dgram.createSocket('udp4');
+  } else {
+    throw Error('Protocol not yet supported');
+  }
+}
+
+function readNext(inputSource, client, args) {
+  var buf = inputSource.read();
+  // parse next line and put remaining chars back into the buffer
+  console.log('read: ', buf);
+  if (buf) {
+    var offset = 0;
+    for (; offset < buf.length; offset++) {
+      if (buf[offset] === '\x0A' || buf[offset] === 'x0D') {
+        const msg = buf.slice(0, offset + 1);
+        client.send(msg, args.port, args.host, (err) => {
+          if ( err ) {
+            console.warn('Message transmission error: ', err);
+          }
+        });
+        buf = buf.slice(offset + 1);
+        inputSource.unshift(buf);
+        break;
+      }
+    }
+  }
+
+  // stdin seems to get paused during the readable callback?
+  // regardless, if any input source is paused here, lets resume
+  if ( inputSource.isPaused() ) {
+    inputSource.resume();
+  }
+
+  if ( !args.interactive ) {
+    timerId = setTimeout(readNext.bind(null,inputSource, client, args), 1000 * args.speed);
+  }
+};
+
+function readNextIfNecessary(inputSource, client, args) {
+  // if we're not rate limited, or we havent read anything yet, read from the input source
+  // otherwise we know we'll check back in shortly to read the data in the buffer
+  if ( !timerId ) {
+    readNext(inputSource, client, args);
+  }
+}
+
+function main() {
+  const args = parser.argv;
+  
+  const client = getClient(args.protocol);
+  const inputSource = args.interactive
+        ? process.stdin
+        : fs.createReadStream(args.file, { encoding: 'utf8', highWaterMark: 1024 });
+  inputSource.on('readable', readNextIfNecessary.bind(null, inputSource, client, args));
+  inputSource.on('close', () => clearTimeout(timerId));
+}
+
+main();
